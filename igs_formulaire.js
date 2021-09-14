d6916b049482ee24ae9800804f66aa218f65a6dd
Jean-joel <90659970+Jean-joel@users.noreply.github.com>
2021-09-14 02:37:01 +0200

    Add files via upload

---
+++ igs_formulaire.js
@@ -0,0 +1,27 @@
+var tableau_formulaire=[
+ /*0*/   formulaire= document.createElement('form'),
+  /*1*/  fieldset=document.createElement('fieldset'),
+  /*2*/  legend=document.createElement('legend'),
+  /*3*/  titre_formulaire=document.createElement('h1'),
+   /*4*/ label_nom_utilisateur=document.createElement('label'),
+  /*5*/  nom_utilisateur=document.createElement('input'),
+  /*6*/  label_prenom_utilisateur=document.createElement('label'),
+   /*7*/ prenom_utilisateur=document.createElement('input'),
+   /*8*/ label_birthday_utilisateur=document.createElement('label'),
+   /*9*/ birthday_utilisateur=document.createElement('input'),
+   /*10*/ intitule_genre_utilisateur=document.createElement('p'),
+   /*11*/ genre_male_utilisateur=document.createElement('input'),
+   /*12*/ genre_femelle_utilisateur=document.createElement('input'),
+   /*13*/ intitule_handicap_utilisateur=document.createElement('p'),
+    /*14*/handicap_oui_utilisateur=document.createElement('input'),
+    /*15*/handicap_non_utilisateur=document.createElement('input'),
+   /*16*/ nature_handicap_utilisateur=document.createElement('textarea'),
+    /*17*/label_telephone_utilisateur=document.createElement('label'),
+    /*18*/indice_tel_utilisateur=document.createElement('input'),
+    /*19*/tel_utilisateur=document.createElement('input'),
+    /*20*/bouton_soumettr=document.createElement('input')
+];  
+
+
+
+//alert("ok"); 
